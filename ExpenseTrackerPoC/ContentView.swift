4702bdfaed3da9194323fa2fb9d7055984874ca2
Víctor Hugo Valle Castillo <victouk@gmail.com>
2025-05-08 17:16:48 -0400

    Bootstrap the project with SwiftUI.

---
+++ ExpenseTrackerPoC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerPoC
+//
+//  Created by Víctor Hugo Valle Castillo on 2025-05-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ ExpenseTrackerPoC/ExpenseTrackerPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerPoCApp.swift
+//  ExpenseTrackerPoC
+//
+//  Created by Víctor Hugo Valle Castillo on 2025-05-08.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerPoCApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
