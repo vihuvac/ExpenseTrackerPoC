e550018321513c7e1a86c478b21a247cf7b442a2
Víctor Hugo Valle Castillo <victouk@gmail.com>
2025-05-09 19:38:27 -0400

    Fixed tuple type mismatch in SummaryView.sortedCategories and resolved cos ambiguity with Foundation import

---
+++ ExpenseTrackerPoC/Views/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  ExpenseTrackerPoC
+//
+//  Created by Víctor Hugo Valle Castillo on 2025-05-09.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
